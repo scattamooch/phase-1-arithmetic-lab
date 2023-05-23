5cf31e6e88291b539bed18c94890e464fa724bf4
scattamooch <seanstevens729@gmail.com>
2023-05-23 13:44:21 -0400

    edited index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+//multiply
+const num1 = 31
+const num2 = 2
+function multiplication(num1, num2) {
+    return num1*num2;
+}
+let multiply = multiplication(num1, num2)
 
-// Write your code here
+//random
+function rng() {
+    return Math.floor(Math.random()*10 + 1);
+}
+let random = rng()
+
+//mod
+const num3 = 40
+const num4 = 9
+function remainder(num3, num4) {
+    return 40 % 9;
+}
+let mod = remainder(num3, num4)
+
+//max
+const max = Math.max(0,5,10,15,20)
